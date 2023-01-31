c1016d977118a29b65b64e5e85bb363894e86c26
Brent <BrentHickman@ymail.com>
2023-01-31 14:17:39 -0800

    contact passes first test

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,7 @@
 export default function Contact(addressName, addressAddress, addressPhone, workEmail, personalEmail){
-  // this.addressName = addressName;
-  // this.addressAddress = addressAddress;
-  // this.addressPhone = addressPhone;
-  // this.workEmail = workEmail;
-  // this.personalEmail = personalEmail;
+  this.addressName = addressName;
+  this.addressAddress = addressAddress;
+  this.addressPhone = addressPhone;
+  this.workEmail = workEmail;
+  this.personalEmail = personalEmail;
 }
